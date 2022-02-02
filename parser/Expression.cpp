6ccf9f70b6f0b46d2ddd42b09ea61dda847f843b
MatousAc <matoush@southern.edu>
2022-02-01 23:32:56 -0500

    generating works

--- parser/Expression.cpp
+++
@@ -1,8 +0,0 @@
-#include "Expression.h"
-Visitor::Visitor(Expression* obj) : m_obj(obj) {}
-
-
-void Visitable::accept(Expression& visitor) {
-    Visitor wrapped(&visitor);
-    accept_visitor(wrapped);
-}
