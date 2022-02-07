c3ec882ed1be8d80c6768fb31e572974a08484c7
MatousAc <matoush@southern.edu>
2022-02-06 20:30:09 -0500

    RPN Printer

---
+++ tools/RPNPrinter.cpp
@@ -0,0 +1,37 @@
+#include "RPNPrinter.h"
+void RPNPrinter::read(Expression* expression) {
+	expression->accept(this);
+}
+
+void RPNPrinter::visitBinary(const Binary* expression) {
+	collect(
+		expression->op.lexeme,
+		vector{ expression->left, expression->right }
+	);
+}
+void RPNPrinter::visitGrouping(const Grouping* expression) {
+	expression->expression->accept(this);
+}
+
+void RPNPrinter::visitLit(const Lit* expression) {
+	if (expression->value.empty()) result += "nil";
+	else result += expression->value.toString();
+}
+
+void RPNPrinter::visitUnary(const Unary* expression) {
+	collect(expression->op.lexeme, vector{ expression->right });
+}
+
+string RPNPrinter::collect(string name, vector<Expression*> expressions) {
+	auto cur = expressions.begin();
+	auto end = expressions.end();
+	while (cur != end) {
+		result += " ";
+		(*cur)->accept(this);
+		cur++;
+	}
+	result += " " + name;
+	return result;
+}
+
+string RPNPrinter::getResult() { return result; }
---
+++ tools/RPNPrinter.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../include.h"
+#include "../parser/Expression.hpp"
+
+class RPNPrinter : Visitor {
+	string result;
+public:
+	RPNPrinter() : result{} {};
+	void read(Expression* expression);
+
+	void visitBinary(const Binary* expression) override;
+	void visitGrouping(const Grouping* expression) override;
+	void visitLit(const Lit* expression) override;
+	void visitUnary(const Unary* expression) override;
+
+	string collect(string name, vector<Expression*> expressions);
+
+	string getResult();
+};
+
